88700b0781917fb8910f9a9f21d143fb1390c72c
Alec M Khoury <akhoury.ak@gmail.com>
2019-01-26 16:10:49 -0600

    configuring postCreation

--- components/createPost.vue
+++ components/createPost.vue
@@ -2,144 +2,146 @@
     <section class="section">
         <div class="box level">
             <span class="level-left is-size-4"><b>Create a post...</b></span>
-            <a class="button" @click="createDialog = !createDialog"><b>Create</b></a>
+            <a class="button action" @click="createDialog = !createDialog"><b>Create</b></a>
         </div>
-        <div v-if="createDialog" class="modal is-active">
+        <div class="modal" :class="{ 'is-active': createDialog }">
             <div class="modal-background"></div>
             <div class="modal-card">
-                <section class="modal-card-body">
-                    <figure class="media-left">
+                <section class="modal-card-body columns">
+                    <figure class="column is-paddingless is-1">
                         <p class="image is-64x64 profilePic">
                             <img :src="this.$store.state.user.profilePicture">
                         </p>
                     </figure>
-                    <div class="editor">
-                        <editor-menu-bar :editor="editor">
-                            <div class="menubar" slot-scope="{ commands, isActive }">
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.bold() }"
-                                        @click="commands.bold"
-                                >
-                                    <icon name="bold" />
-                                </button>
+                    <div class="column">
+                    <editor-menu-bar :editor="editor">
+                        <div class="field has-addons" slot-scope="{ commands, isActive }">
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.bold() }"
+                                    @click="commands.bold"
+                            >
+                                <font-awesome-icon icon="bold" />
+                            </a>
 
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.italic() }"
-                                        @click="commands.italic"
-                                >
-                                    <icon name="italic" />
-                                </button>
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.italic() }"
+                                    @click="commands.italic"
+                            >
+                                <font-awesome-icon icon="italic" />
+                            </a>
 
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.strike() }"
-                                        @click="commands.strike"
-                                >
-                                    <icon name="strike" />
-                                </button>
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.strike() }"
+                                    @click="commands.strike"
+                            >
+                                <font-awesome-icon icon="strikethrough" />
+                            </a>
 
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.underline() }"
-                                        @click="commands.underline"
-                                >
-                                    <icon name="underline" />
-                                </button>
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.underline() }"
+                                    @click="commands.underline"
+                            >
+                                <font-awesome-icon icon="underline" />
+                            </a>
 
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.code() }"
-                                        @click="commands.code"
-                                >
-                                    <icon name="code" />
-                                </button>
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.code() }"
+                                    @click="commands.code"
+                            >
+                                <font-awesome-icon icon="code" />
+                            </a>
 
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.paragraph() }"
-                                        @click="commands.paragraph"
-                                >
-                                    <icon name="paragraph" />
-                                </button>
+                            <!--<a-->
+                            <!--class="button is-white"-->
+                            <!--:class="{ 'is-active': isActive.paragraph() }"-->
+                            <!--@click="commands.paragraph"-->
+                            <!--&gt;-->
+                            <!--<font-awesome-icon icon="paragraph" />-->
+                            <!--</a>-->
 
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.heading({ level: 1 }) }"
-                                        @click="commands.heading({ level: 1 })"
-                                >
-                                    H1
-                                </button>
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+                                    @click="commands.heading({ level: 1 })"
+                            >
+                                H1
+                            </a>
 
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.heading({ level: 2 }) }"
-                                        @click="commands.heading({ level: 2 })"
-                                >
-                                    H2
-                                </button>
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.heading({ level: 3 }) }"
-                                        @click="commands.heading({ level: 3 })"
-                                >
-                                    H3
-                                </button>
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.bullet_list() }"
-                                        @click="commands.bullet_list"
-                                >
-                                    <icon name="ul" />
-                                </button>
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.ordered_list() }"
-                                        @click="commands.ordered_list"
-                                >
-                                    <Icon name="ol" />
-                                </button>
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.blockquote() }"
-                                        @click="commands.blockquote"
-                                >
-                                    <icon name="quote" />
-                                </button>
-                                <button
-                                        class="menubar__button"
-                                        :class="{ 'is-active': isActive.code_block() }"
-                                        @click="commands.code_block"
-                                >
-                                    <icon name="code" />
-                                </button>
-                                <button
-                                        class="menubar__button"
-                                        @click="commands.horizontal_rule"
-                                >
-                                    <icon name="hr" />
-                                </button>
-                                <button
-                                        class="menubar__button"
-                                        @click="commands.undo"
-                                >
-                                    <icon name="undo" />
-                                </button>
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+                                    @click="commands.heading({ level: 2 })"
+                            >
+                                H2
+                            </a>
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+                                    @click="commands.heading({ level: 3 })"
+                            >
+                                H3
+                            </a>
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.bullet_list() }"
+                                    @click="commands.bullet_list"
+                            >
+                                <font-awesome-icon icon="list-ul" />
+                            </a>
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.ordered_list() }"
+                                    @click="commands.ordered_list"
+                            >
+                                <font-awesome-icon icon="list-ol" />
+                            </a>
+                            <a
+                                    class="button is-white"
+                                    :class="{ 'is-active': isActive.blockquote() }"
+                                    @click="commands.blockquote"
+                            >
+                                <font-awesome-icon icon="quote-right" />
+                            </a>
+                            <!--<a-->
+                            <!--class="button is-white is-white"-->
+                            <!--:class="{ 'is-active': isActive.code_block() }"-->
+                            <!--@click="commands.code_block"-->
+                            <!--&gt;-->
+                            <!--<font-awesome-icon icon="code" />-->
+                            <!--</a>-->
+                            <a
+                                    class="button is-white"
+                                    @click="commands.horizontal_rule"
+                            >
+                                <font-awesome-icon icon="minus" />
+                            </a>
+                            <!--<a-->
+                            <!--class="button is-white"-->
+                            <!--@click="commands.undo"-->
+                            <!--&gt;-->
+                            <!--<font-awesome-icon icon="undo" />-->
+                            <!--</a>-->
 
-                                <button
-                                        class="menubar__button"
-                                        @click="commands.redo"
-                                >
-                                    <icon name="redo" />
-                                </button>
-                            </div>
-                        </editor-menu-bar>
-                        <editor-content class="editor__content" :editor="editor" />
+                            <!--<a-->
+                            <!--class="button is-white"-->
+                            <!--@click="commands.redo"-->
+                            <!--&gt;-->
+                            <!--<font-awesome-icon icon="redo" />-->
+                            <!--</a>-->
+                        </div>
+                    </editor-menu-bar>
+                        <div class="editor content">
+                            <editor-content class="editor__content" :editor="editor" />
+                    </div>
                     </div>
                 </section>
                 <footer class="modal-card-foot">
-                    <button class="button" @click="createDialog = !createDialog">Cancel</button>
+                    <a class="button action" @click="createDialog = !createDialog">Cancel</a>
                 </footer>
             </div>
         </div>
@@ -147,7 +149,6 @@
 </template>
 
 <script>
-  import Icon from './icon.vue';
   import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
   import {
     Blockquote,
@@ -175,13 +176,14 @@
       Editor,
       EditorContent,
       EditorMenuBar,
-      Icon
     },
     data() {
       return {
         createDialog: false,
         editor: null
       };
+    },
+    methods: {
     },
     beforeDestroy() {
       this.editor.destroy()
@@ -220,9 +222,25 @@
 span {
     color: #39C9A0;
 }
-.button {
+.modal-card {
+    width: 55%;
+    border-radius: 6px;
+}
+.button.action {
     width: 12em;
     color: white;
     background-color: #39C9A0;
+}
+.is-white:hover {
+    background-color: #E9EBEE;
+}
+.editor__content {
+    padding-left: 1em;
+    height: 5em;
+    border-color: transparent;
+    box-sizing: border-box;
+}
+.editor__content:focus {
+    outline: 0;
 }
 </style>
