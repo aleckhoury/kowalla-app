1a66cfae0f24cfeae882d4590195658bec0be861
Alec M Khoury <akhoury.ak@gmail.com>
2019-04-13 12:07:00 -0500

    small change to load info on reload

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -3,9 +3,7 @@ import createPersistedState from 'vuex-persistedstate';
 export default ({store, app, $axios }) => {
   window.onNuxtReady(async () => {
     createPersistedState()(store);
-    if (!store.state.user.loggedIn) {
-      store.commit('user/clearUser', { loggedIn: false });
-    } else {
+    if (store.state.user.loggedIn) {
       const user = await $axios.$get(`api/v1/users/${ store.state.user.username }`);
       const subs = await $axios.$get(`/api/v1/profiles/${ user._id }/subs`);
 
