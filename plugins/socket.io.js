447764a5faf7cbc36f045ed0425656c0093bcc0b
Alec M Khoury <akhoury.ak@gmail.com>
2019-07-21 13:17:56 -0500

    Revert change since it's working now

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -3,5 +3,5 @@ import io from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io-extended';
 
 export default ({ store }) => {
-  Vue.use(VueSocketIO, io('https://kowalla-backend-tob.herokuapp.com:80'), { store });
+  Vue.use(VueSocketIO, io('https://kowalla-backend-tob.herokuapp.com'), { store });
 };
