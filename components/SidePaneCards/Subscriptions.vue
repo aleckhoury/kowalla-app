66a2d43ddd6f752504f35431cae5312e79df4b3b
Alec M Khoury <akhoury.ak@gmail.com>
2019-10-02 20:08:11 -0500

    A few more sidepane card testing changes

--- components/SidePaneCards/Subscriptions.vue
+++ components/SidePaneCards/Subscriptions.vue
@@ -1,16 +1,16 @@
 <template>
-  <CardContainer>
-    <div v-for="sub in subscriptions" :key="sub._id" class="nav-card-link-container">
-      <nuxt-link :to="getRoute">
-        <img :src="sub.pictureUrl" class="picture">
+  <CardContainer header-string="Subscriptions">
+    <div v-for="sub in subscriptions" :key="sub._id" class="subscription">
+      <nuxt-link :to="getRoute(sub.isProject)">
+        <img :src="sub.pictureUrl" />
       </nuxt-link>
 
-      <div class="nav-card-link-content">
+      <div class="subscription-link">
         <nuxt-link :to="getRoute(sub.isProject)" class="name">
-          <b>{{ getPrefix(sub.isProject) }}{{ name }}</b>
+          <b>{{ getPrefix(sub.isProject) }}{{ sub.spaceName }}</b>
         </nuxt-link>
 
-        <div class="info">{{ numSubs }} users</div>
+        <!--        <div v-if="" class="info">{{ numSubs }} users</div>-->
       </div>
     </div>
   </CardContainer>
@@ -19,24 +19,65 @@
 <script>
 import CardContainer from "./CardContainer";
 
-  export default {
-    name: "Subscriptions",
-    components: { CardContainer },
-    computed: {
-      subscriptions() {
-        return this.$store.state.user.subscriptions;
-      },
+export default {
+  name: "Subscriptions",
+  components: { CardContainer },
+  computed: {
+    subscriptions() {
+      return this.$store.state.user.subscriptions;
     },
-    methods: {
-      getRoute(isProject) {
-        return isProject ? `/beta/project/${this.name}` : `/beta/space/${this.name}`;
-      },
-      getPrefix(isProject) {
-        return isProject ? "@" : "#";
-      },
+  },
+  methods: {
+    getRoute(isProject) {
+      return isProject
+        ? `/beta/project/${this.name}`
+        : `/beta/space/${this.name}`;
     },
-  };
+    getPrefix(isProject) {
+      return isProject ? "@" : "#";
+    },
+  },
+};
 </script>
 
 <style scoped>
+.subscription {
+  height: 36px;
+  display: flex;
+  flex-direction: row;
+  margin: 8px;
+}
+img {
+  height: 36px;
+  width: 36px;
+  border-radius: 6px;
+  margin-left: 8px;
+  cursor: pointer;
+}
+.subscription-link {
+  display: flex;
+  flex-direction: column;
+  margin-left: 8px;
+}
+.name {
+  font-family: "Helvetica Neue";
+  font-size: 1em;
+  height: 50%;
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.name:hover {
+  text-decoration: underline;
+  color: #39c9a0;
+}
+
+.info {
+  font-family: "Helvetica Neue";
+  font-size: 0.75em;
+  margin-top: 2px;
+  height: 50%;
+  color: #999;
+}
 </style>
