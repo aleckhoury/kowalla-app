33762b11081fdee1ea2aa97e74b6a55afc532f1f
Alec M Khoury <akhoury.ak@gmail.com>
2020-02-08 23:12:45 -0600

    Fix emoji reactions

--- components/PostCards/DropdownPicker.vue
+++ components/PostCards/DropdownPicker.vue
@@ -1,19 +1,31 @@
 <template>
-  <Picker :show-skin-tones="false" :show-preview="false" @select="toggleReaction" />
+  <Picker :data="index" :show-skin-tones="false" :show-preview="false" @select="toggleReaction" />
 </template>
 
 <script>
 let Mart;
+let data;
+let index;
 if (process.browser) {
   Mart = require('emoji-mart-vue-fast');
+  data = require('emoji-mart-vue-fast/data/all.json');
 }
-let { Picker } = Mart ? Mart : '';
+let { Picker, EmojiIndex } = Mart ? Mart : '';
+if (data) {
+  index = new EmojiIndex(data);
+}
 
 export default {
   name: 'DropdownPicker',
   components: { Picker },
   props: {
     toggleReaction: { type: Function, default: () => {} },
+  },
+  data() {
+    return {
+      data,
+      index,
+    };
   },
 };
 </script>
